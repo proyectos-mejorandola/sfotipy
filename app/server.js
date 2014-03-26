9f5229ab17a467d8ca5af7812b1c0ff71867a32e
Julian Duque <julianduquej@gmail.com>
2014-03-26 15:08:34 -0500

    Eliminar archivos innecesarios

--- app/server.js
+++
@@ -1,8 +0,0 @@
-var http = require('http'),
-    st   = require('node-static'),
-    opts = { },
-    file = new st.Server('./public', opts);
-
-http.createServer(function (req, res) {
-    file.serve(req, res);
-}).listen(process.env.PORT || 8080)
