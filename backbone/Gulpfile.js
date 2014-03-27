b46b7a55f9d96119a3e6d633aecf2c9e88b6e241
Julian Duque <julianduquej@gmail.com>
2014-03-27 14:30:43 -0500

    Add Gulp

--- backbone/Gulpfile.js
+++ backbone/Gulpfile.js
@@ -2,7 +2,8 @@ var gulp        = require('gulp'),
     uglify      = require('gulp-uglify'),
     changed     = require('gulp-changed')
     imagemin    = require('gulp-imagemin'),
-    stripDebug  = require('gulp-strip-debug')
+    stripDebug  = require('gulp-strip-debug'),
+    minifyCSS   = require('gulp-minify-css'),
     minifyHTML  = require('gulp-minify-html'),
     browserify  = require('gulp-browserify');
 
@@ -12,6 +13,12 @@ gulp.task('js', function () {
     .pipe(uglify({ compress: true }))
     .pipe(stripDebug())
     .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('css', function () {
+  gulp.src('app/css/**/*.css')
+    .pipe(minifyCSS({ keepSpecialComments: '*', keepBreaks: '*'}))
+    .pipe(gulp.dest('./public/css'));
 });
 
 gulp.task('images', function () {
@@ -33,4 +40,14 @@ gulp.task('html', function () {
   .pipe(gulp.dest(htmlDst));
 });
 
-gulp.task('default', [ 'js', 'images', 'html' ]);
+gulp.task('fonts', function () {
+  gulp.src('app/fonts/**')
+    .pipe(gulp.dest('./public/fonts'));
+});
+
+gulp.task('data', function () {
+   gulp.src('app/data.json')
+    .pipe(gulp.dest('./public'));
+});
+
+gulp.task('default', [ 'js', 'css', 'images', 'html', 'fonts', 'data' ]);
