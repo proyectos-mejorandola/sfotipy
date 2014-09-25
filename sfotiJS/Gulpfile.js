ce33f6c6cbb604e7632d56323724c25f7278654d
Carlos Azaustre <cazaustre@gmail.com>
2014-09-25 10:01:36 +0200

    wire dependencies via bower

--- sfotiJS/Gulpfile.js
+++ sfotiJS/Gulpfile.js
@@ -3,7 +3,11 @@
 var gulp                = require('gulp');
 var connect             = require('gulp-connect');
 var nodemon             = require('gulp-nodemon');
-var wiredep             = require('wiredep').stream();
+var wiredep             = require('wiredep').stream;
+var inject              = require('gulp-inject');
+var stylus              = require('gulp-stylus');
+var nib                 = require('nib');
+var angularFilesort     = require('gulp-angular-filesort');
 var historyApiFallback  = require('connect-history-api-fallback');
 var $                   = require('gulp-load-plugins')();
 
@@ -20,18 +24,44 @@ gulp.task('development', function () {
   });
 });
 
-// Inyect dependencies on HTML via Bower
+// Inject dependencies on HTML via Bower
 gulp.task('wiredep', function () {
-  gulp.src('client/index.html')
+  gulp.src('./client/index.html')
     .pipe(wiredep({
-      directory: 'client/lib'
+      directory: './client/lib'
     }))
+    .pipe(gulp.dest('./client'));
+});
+
+// Process Stylus files to CSS
+gulp.task('css', function () {
+  gulp.src('./client/css/main.styl')
+    .pipe(stylus({ use: nib() }))
+    .pipe(gulp.dest('./client/css'));
+});
+
+// Inject CSS and JS files into HTML
+gulp.task('inject', function () {
+  return gulp.src('index.html', { cwd: 'client' })
+    .pipe(inject(
+      gulp.src(['client/js/**/*.js']).pipe(angularFilesort()), {
+        read: false,
+        ignorePath: '/client'
+      }))
+    .pipe(inject(
+      gulp.src(['client/css/**/*.css']), {
+        read: false,
+        ignorePath: '/client'
+      }
+    ))
     .pipe(gulp.dest('client'));
 });
 
 // Watch the changes and trigger the tasks
 gulp.task('watch', function () {
-  gulp.watch(['bower.json'], ['wiredep']);
+  gulp.watch(['./client/css/**/*.styl'], ['css', 'inject']);
+  gulp.watch(['./client/js/**/*.js'], ['inject']);
+  gulp.watch(['./bower.json'], ['wiredep']);
 });
 
-gulp.task('default', ['development', 'watch']);
+gulp.task('default', ['development', 'inject', 'wiredep', 'watch']);
